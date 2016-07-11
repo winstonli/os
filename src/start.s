f3329b5b60994ce43a1224b5ddc5f4783870819b
George Steed <10steedgd1@gmail.com>
2016-07-11 16:33:53 +0100

    add more comments, print 'Hi' to check it's actually working

--- src/start.s
+++ src/start.s
@@ -20,21 +20,20 @@ TAG_REQUIRED equ 0
 TAG_OPTIONAL equ 1
 
 HEADER_ADDR equ multiboot_header ; TODO hardcoded magic, get from linker script
-LOAD_ADDR equ start ; TODO this could be wrong!
-LOAD_END_ADDR equ 0 ; TODO
-BSS_ADDR equ 0 ; TODO
-
-
+LOAD_ADDR equ start ; TODO this could be wrong if start is not at the beginning of the text/data
+LOAD_END_ADDR equ 0 ; TODO with this as 0, the whole file is assumed to be text/data so bss will not be handled correctly
+BSS_ADDR equ 0 ; TODO we currently have no bss segment
 
 section .multiboot
 align 8, db 0 ; pad with 0s to align to 64-bit boundary
 multiboot_header:
-  dd MAGIC
-  dd ARCHITECTURE
+  dd MAGIC ; magic number so grub2 will find us
+  dd ARCHITECTURE ; x86, 32-bit protected mode
   dd HEADER_LENGTH
-  dd CHECKSUM
+  dd CHECKSUM ; must be 0 when above three fields are added
+
 .tags:
-align 8, db 0 ; pad with 0s to align to 64-bit boundary
+align 8, db 0
 .addr_tag:
   dw TAG_TYPE_ADDRESS
   dw TAG_OPTIONAL
@@ -44,23 +43,25 @@ align 8, db 0 ; pad with 0s to align to 64-bit boundary
   dd LOAD_END_ADDR
   dd BSS_ADDR
 .addr_tag_end:
-align 8, db 0 ; pad with 0s to align to 64-bit boundary
+align 8, db 0
 .entry_tag:
   dw TAG_TYPE_ENTRY
   dw TAG_OPTIONAL
   dd (.entry_tag_end - .entry_tag)
   dd start
 .entry_tag_end:
-align 8, db 0 ; pad with 0s to align to 64-bit boundary
-.framebuffer_tag:
-  dw TAG_TYPE_FRAMEBUFFER
-  dw TAG_OPTIONAL
-  dd (.framebuffer_tag_end - .framebuffer_tag)
-  dd 1024
-  dd 768
-  dd 32
-.framebuffer_tag_end:
-align 8, db 0 ; pad with 0s to align to 64-bit boundary
+align 8, db 0
+; Makes us a nice screen to work with, but 0x8b000 is easier for debugging
+; purposes for the time being, so we'll uncomment this later if we want it.
+; .framebuffer_tag:
+;   dw TAG_TYPE_FRAMEBUFFER
+;   dw TAG_OPTIONAL
+;   dd (.framebuffer_tag_end - .framebuffer_tag)
+;   dd 1024
+;   dd 768
+;   dd 32
+; .framebuffer_tag_end:
+; align 8, db 0
 .null_tag:
   dw TAG_TYPE_NULL
   dw 0
@@ -81,11 +82,12 @@ _start:
 ;
 ;    call kernel_main ; Call C kernel
 
-    cli
+  cli
+  mov dword [0xb8000], 0x07690748
 
 hang:
-    hlt
-    jmp hang
+  hlt
+  jmp hang
 
 ;section .bss
 ;align 4
