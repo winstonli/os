b04d3afc7f746d806da997ee36dcf4211afa5cc0
George Steed <10steedgd1@gmail.com>
2016-07-11 23:22:23 +0100

    add bash script to install os to usb

---
+++ install_usb.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+  echo
+  echo "Usage: $0 /dev/sdX"
+  echo
+  exit 1
+fi
+
+drive_label="OSDev-$RANDOM"
+mount_dir="/media/os-$RANDOM"
+
+# wipe the existing drive and make a new partition table
+sudo fdisk "$1" <<EOF
+o
+n
+p
+1
+
+
+w
+EOF
+
+# format the partition as FAT32 and mount it
+sudo mkdir -p "$mount_dir"
+sudo mkfs.vfat -F 32 -n "$drive_label" -I "$1"1
+sudo mount "$1"1 "$mount_dir"
+
+# install grub to the drive
+sudo grub-install --root-directory="$mount_dir" --no-floppy --recheck --force "$1"
+
+# copy in kernel and grub config
+sudo cp kernel "$mount_dir/boot/"
+sudo cp grub.cfg "$mount_dir/boot/grub/"
+
+# ensure data is written, then unmount and eject device
+sudo sync
+sudo umount "$1"1
+sudo udisksctl power-off --block-device "$1"
+
+# cleanup
+sudo rm -rf "$mount_dir"
