f509766ba068589ad8594085d6fc719d0306606f
Winston Li <winston.li12@imperial.ac.uk>
2016-07-25 19:05:55 +0100

    - fixedsize_vector.h
        Add emplace_back and pop_back and fix other functions to
        support destruction.

--- src/modules/kernel/util/fixedsize_vector.h
+++ src/modules/kernel/util/fixedsize_vector.h
@@ -6,27 +6,36 @@
 template<typename T, std::size_t N>
 class fixedsize_vector {
 
-  std::array<T, N> arr;
+  std::array<char, sizeof(T) * N> arr;
   std::size_t _size;
 
 public:
 
-  T &operator[](size_t i) { return arr[i]; }
+  T &operator[](size_t i) { return data()[i]; }
 
-  const T &operator[](size_t i) const { return arr[i]; }
+  const T &operator[](size_t i) const { return data()[i]; }
 
   size_t size() { return _size; }
 
-  const T *data() const { return arr.data(); }
+  const T *data() const { return reinterpret_cast<const T *>(arr.data()); }
 
-  T *data() { return arr.data(); }
+  T *data() { return reinterpret_cast<T *>(arr.data()); }
 
   void push_back(const T &val) {
-    arr[_size++] = val;
+    arr[_size++] = T(val);
   }
 
   void push_back(T &&val) {
-    arr[_size++] = std::move(val);
+    arr[_size++] = T(std::move(val));
+  }
+
+  template <class... _Args>
+  void emplace_back(_Args&&... __args) {
+    arr[_size++] = T(std::forward<_Args>(__args)...);
+  }
+
+  void pop_back() {
+    arr[_size--].~T();
   }
 
 };
