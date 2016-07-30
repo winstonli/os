ef510f1d39d3e67741906b53fd179243fd83f5ae
George Steed <10steedgd1@gmail.com>
2016-07-30 02:09:07 +0100

    search through memory for the acpi and mp tables

--- src/modules/kernel/main.cpp
+++ src/modules/kernel/main.cpp
@@ -29,6 +29,36 @@
 extern int64_t link_kern_start;
 extern int64_t link_kern_end;
 
+// 16-bit pointer to ebda start physical addr right-shifted 4 places
+// (so shift left 4 to get actual physical addr)
+#define EBDA_PTR_PADDR reinterpret_cast<void *>(0x040e)
+
+static void *get_ebda_addr() {
+  auto ebda_ptr_vaddr = vm::paddr_to_vaddr(EBDA_PTR_PADDR);
+  auto ebda_paddr = *reinterpret_cast<uint16_t *>(ebda_ptr_vaddr) << 4;
+  return vm::paddr_to_vaddr(reinterpret_cast<void *>(ebda_paddr));
+}
+
+static void search_for_mp(char *begin, char *end) {
+  // check is 16-byte aligned
+  assert(reinterpret_cast<uintptr_t>(begin) % 16 == 0);
+  for (; begin < end; begin += 16) {
+    if (!memcmp(begin, "_MP_", 4)) {
+      klog_debug("Found MP Floating Pointer Structure at %x", begin);
+    }
+  }
+}
+
+static void search_for_rsdp(char *begin, char *end) {
+  // check is 16-byte aligned
+  assert(reinterpret_cast<uintptr_t>(begin) % 16 == 0);
+  for (; begin < end; begin += 16) {
+    if (!memcmp(begin, "RSD PTR ", 8)) {
+      klog_debug("Found Root System Description Pointer at %x", begin);
+    }
+  }
+}
+
 // entry point of 64-bit kernel proper, as jumped to from entry.s
 extern "C" void kernel_main(const uint32_t multiboot_magic,
                             void *multiboot_data, void *start_mod_start,
@@ -52,6 +82,24 @@ extern "C" void kernel_main(const uint32_t multiboot_magic,
   pci::init();
   lapic::init();
   ioapic::init();
+
+  auto ebda_addr = reinterpret_cast<char *>(get_ebda_addr());
+  klog_debug("Extended BIOS data area is located at %x", ebda_addr);
+
+  // places to search (only 16-byte boundaries):
+  //  - ebda_addr to ebda_addr + 1024
+  //  - 0xE0000 to 0xFFFFF
+
+  auto bios_area_start = reinterpret_cast<char *>(
+      vm::paddr_to_vaddr(reinterpret_cast<void *>(0xe0000)));
+  auto bios_area_end = reinterpret_cast<char *>(
+      vm::paddr_to_vaddr(reinterpret_cast<void *>(0xfffff)));
+
+  search_for_rsdp(ebda_addr, ebda_addr + 1024);
+  search_for_rsdp(bios_area_start, bios_area_end);
+
+  search_for_mp(ebda_addr, ebda_addr + 1024);
+  search_for_mp(bios_area_start, bios_area_end);
 
   uint64_t rip = 0;
   asm volatile("leaq (%%rip), %0" : "=r"(rip));
